f29d78babe3c30e5a2e3d7776647cda66265c79a
Patrik Stutz <patrik.stutz@gmail.com>
2023-02-15 23:04:42 +0100

    give each task in a pipeline a color to easier distinguish their outputs

--- task-graph-processor/src/run.ts
+++ task-graph-processor/src/run.ts
@@ -1,12 +1,40 @@
+import chalk from "chalk";
 import { Signal } from "typed-signals";
+import distinctColors from "distinct-colors";
 import { TaskItem } from "./taskfile.js";
+
+type PipelineItem = {
+  task: TaskItem;
+  color: string;
+  dependencies: PipelineItem[];
+  dependents: PipelineItem[];
+};
 
 export const createPipeline = (
   tasks: TaskItem[],
   options: PipelineOptions
 ): Pipeline => {
+  const colors = distinctColors
+    .default({ count: tasks.length })
+    .map((c) => c.hex());
+  const items: PipelineItem[] = tasks.map((task, index) => ({
+    task,
+    color: colors[index],
+    dependencies: [],
+    dependents: [],
+    state: { type: "PENDING" },
+  }));
+  for (const item of items) {
+    item.dependencies = item.task.dependencies.map(
+      (dependency) => items.find((item) => item.task === dependency)!
+    );
+    item.dependents = item.task.dependents.map(
+      (dependent) => items.find((item) => item.task === dependent)!
+    );
+  }
+
   const pipeline: Pipeline = {
-    tasks,
+    items,
     options,
     onFinish: new Signal(),
     start: () => {
@@ -18,15 +46,15 @@ export const createPipeline = (
       }
     },
   };
-  tasks.forEach((task) => {
-    task.onOutput.connect((line) => printTaskLine(task, line));
-    if (task.kind === "task") {
-      task.onChange.connect(() => {
-        markDirty(task, pipeline);
+  items.forEach((item) => {
+    item.task.onOutput.connect((line) => printTaskLine(item, line));
+    if (item.task.kind === "task") {
+      item.task.onChange.connect(() => {
+        markDirty(item, pipeline);
         decideAction(pipeline);
       });
-      task.onFinish.connect((success) => {
-        markCompleted(task, success, pipeline);
+      item.task.onFinish.connect((success) => {
+        markCompleted(item, success, pipeline);
       });
     }
   });
@@ -39,54 +67,55 @@ type PipelineOptions = {
 };
 
 type Pipeline = {
-  tasks: TaskItem[];
+  items: PipelineItem[];
   options: PipelineOptions;
   onFinish: Signal<(success: boolean) => void>;
   start: () => void;
 };
 
-const printTaskLine = (task: TaskItem, text: string) => {
-  console.log(`${task.id}> ${text}`);
+const printTaskLine = (item: PipelineItem, text: string) => {
+  console.log(`${chalk.hex(item.color).bold(item.task.id)}> ${text}`);
 };
 
-const getBuildableProjects = (pipeline: Pipeline): TaskItem[] => {
-  return pipeline.tasks.filter(
-    (task) =>
-      task.state.type === "PENDING" &&
-      !task.dependencies.find(
+const getBuildableItems = (pipeline: Pipeline): PipelineItem[] => {
+  return pipeline.items.filter(
+    (item) =>
+      item.task.state.type === "PENDING" &&
+      !item.dependencies.find(
         (dependency) =>
-          (dependency.kind === "task" &&
-            dependency.state.type !== "COMPLETE") ||
-          (dependency.kind === "service" && dependency.state.type === "PENDING")
+          (dependency.task.kind === "task" &&
+            dependency.task.state.type !== "COMPLETE") ||
+          (dependency.task.kind === "service" &&
+            dependency.task.state.type === "PENDING")
       )
   );
 };
 
-const markDirty = (task: TaskItem, pipeline: Pipeline) => {
-  task.state =
-    task.state.type === "RUNNING"
-      ? { ...task.state, dirty: true }
+const markDirty = (item: PipelineItem, pipeline: Pipeline) => {
+  item.task.state =
+    item.task.state.type === "RUNNING"
+      ? { ...item.task.state, dirty: true }
       : { type: "PENDING" };
-  for (const dependent of task.dependents) {
+  for (const dependent of item.dependents) {
     markDirty(dependent, pipeline);
   }
 };
 
 const markCompleted = (
-  task: TaskItem,
+  item: PipelineItem,
   success: boolean,
   pipeline: Pipeline
 ) => {
-  if (task.state.type === "RUNNING") {
-    const time = new Date().getTime() - task.state.start.getTime();
-    task.state = {
+  if (item.task.state.type === "RUNNING") {
+    const time = new Date().getTime() - item.task.state.start.getTime();
+    item.task.state = {
       type: "COMPLETE",
       success,
       time,
     };
     if (success) {
       printTaskLine(
-        task,
+        item,
         `${success ? "succeeded" : "failed"} after ${time}ms`
       );
     }
@@ -94,18 +123,18 @@ const markCompleted = (
   }
 };
 
-const execute = (task: TaskItem, pipeline: Pipeline) => {
-  printTaskLine(task, "starting...");
-  if (task.kind === "task") {
-    task.state = {
+const execute = (item: PipelineItem, pipeline: Pipeline) => {
+  printTaskLine(item, "starting...");
+  if (item.task.kind === "task") {
+    item.task.state = {
       type: "RUNNING",
       dirty: false,
       start: new Date(),
     };
-    task.execute();
-  } else if (task.kind === "service") {
-    task.start();
-    task.state = {
+    item.task.execute();
+  } else if (item.task.kind === "service") {
+    item.task.start();
+    item.task.state = {
       type: "RUNNING",
       dirty: false,
       start: new Date(),
@@ -114,28 +143,28 @@ const execute = (task: TaskItem, pipeline: Pipeline) => {
   }
 };
 
-const isFailed = (task: TaskItem) =>
-  task.state.type === "COMPLETE" && !task.state.success;
+const isFailed = (item: PipelineItem) =>
+  item.task.state.type === "COMPLETE" && !item.task.state.success;
 
-const isDone = (task: TaskItem, pipeline: Pipeline) =>
-  task.kind === "task"
-    ? task.state.type === "COMPLETE" && task.state.success
-    : task.state.type === "RUNNING" || !pipeline.options.watch;
+const isDone = (item: PipelineItem, pipeline: Pipeline) =>
+  item.task.kind === "task"
+    ? item.task.state.type === "COMPLETE" && item.task.state.success
+    : item.task.state.type === "RUNNING" || !pipeline.options.watch;
 
 const decideAction = (pipeline: Pipeline) => {
-  const failed = !!pipeline.tasks.find(isFailed);
+  const failed = !!pipeline.items.find(isFailed);
   if (failed) {
     console.log("at least one task failed, waiting for changes...");
     pipeline.onFinish.emit(false);
     return;
   }
-  const allComplete = !pipeline.tasks.find((t) => !isDone(t, pipeline));
+  const allComplete = !pipeline.items.find((item) => !isDone(item, pipeline));
   if (allComplete) {
     console.log("all tasks completed, waiting for changes...");
     pipeline.onFinish.emit(true);
     return;
   }
-  const buildableProjects = getBuildableProjects(pipeline);
+  const buildableProjects = getBuildableItems(pipeline);
   for (const project of buildableProjects) {
     execute(project, pipeline);
   }
